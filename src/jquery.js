8c0f5c1086b26042e9d7dcee07b919fe0a994cef
lucasFloresya <140627338+lucasFloresya@users.noreply.github.com>
2023-07-27 14:26:37 +0100

    Add files via upload

--- src/jquery.js
+++ src/jquery.js
@@ -1,8 +1,8 @@
 // Function to handle login attempts
 function login() {
   // Get the username and password from the input fields
-  const username = document.getElementById('Bitmasher').value;
-  const password = document.getElementById('Bitmasher').value;
+  const username = document.getElementById('username-a30d').value;
+  const password = document.getElementById('password-a30d').value;
 
   // Check if the entered username and password match the credentials
   if (username === 'Bitmashers' && password === 'Bitmashers123') {
@@ -11,12 +11,14 @@ function login() {
   } else {
     // Decrement the login attempts
     attempts--;
-    // Display an error message
-    alert(`Invalid credentials! You have ${attempts} attempts left.`);
-    
-    // If attempts become zero, disable the login button
-    if (attempts === 0) {
+
+    if (attempts > 0) {
+      // Display an error message with remaining attempts
+      alert(`Invalid credentials! You have ${attempts} attempts left.`);
+    } else {
+      // Disable the login button and display an error message
       document.querySelector('.u-btn-1').setAttribute('disabled', 'true');
+      alert('You have run out of attempts. Please try again later.');
     }
   }
 }
